3dda853100599aa290b8d596f266519a28a44d78
Ziling Chen <78120292+chenziling23@users.noreply.github.com>
2021-12-17 15:00:32 -0800

    resubmit add register unit test and fix register command

--- src/test/java/edu/northeastern/cs5500/starterbot/listeners/commands/RegisterCommandTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/listeners/commands/RegisterCommandTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import edu.northeastern.cs5500.starterbot.controller.DiscordIdController;
 import edu.northeastern.cs5500.starterbot.model.NEUUser;
 import edu.northeastern.cs5500.starterbot.repository.InMemoryRepository;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,9 @@ public class RegisterCommandTest {
 
     @BeforeEach
     void initialize() {
+        student = new NEUUser("Lisa", "nuid111", "discordId111");
+        student.setStaff(false);
+
         discordIdController = new DiscordIdController(new InMemoryRepository<NEUUser>());
         discordIdController.isDiscordIdRegistered("discordId111");
         registerCommand = new RegisterCommand(discordIdController);
@@ -43,11 +48,14 @@ public class RegisterCommandTest {
 
     @Test
     void testComeBackUser() {
-        MessageEmbed messageEmbed =
-                registerCommand.createAUser("Lisa", "nuid111", "student", "discordId111");
-        assertThat(messageEmbed).isNotNull();
-        assertThat(messageEmbed.getTitle()).isNotEmpty();
-        assertThat(messageEmbed.getFields()).isNotEmpty();
+        MessageBuilder mb1 = new MessageBuilder();
+        EmbedBuilder eb1 = new EmbedBuilder();
+        eb1.setTitle(String.format("Welcome back %s:", student.getUserName()));
+        eb1.setDescription("You have already been registered");
+        mb1.setEmbed(eb1.build());
+        assertEquals(
+                registerCommand.getReply(student, "Lisa", "nuid111", "student", "discordId111"),
+                mb1.build());
     }
 
     @Test
@@ -56,13 +64,38 @@ public class RegisterCommandTest {
     }
 
     @Test
-    void testGetCommandData() {
-        assertThat(registerCommand.getCommandData()).isNotNull();
+    void testGetName2() {
+        assertEquals(registerCommand.getName(), "register");
     }
 
     @Test
-    void testGetCommandDataIsConsistent() {
-        assertThat(registerCommand.getCommandData().getName())
-                .isEqualTo(registerCommand.getName());
+    void testGetCommandData1() {
+        assertEquals(
+                registerCommand.getCommandData().getDescription(),
+                "register yourself with the bot");
+    }
+
+    @Test
+    void testGetCommandData2() {
+        assertEquals(registerCommand.getCommandData().getOptions().get(0).getName(), "name");
+        assertEquals(
+                registerCommand.getCommandData().getOptions().get(0).getDescription(),
+                "The name you would like to be referred to as");
+    }
+
+    @Test
+    void testGetCommandData3() {
+        assertEquals(registerCommand.getCommandData().getOptions().get(1).getName(), "nuid");
+        assertEquals(
+                registerCommand.getCommandData().getOptions().get(1).getDescription(),
+                "Your NUID (numbers only)");
+    }
+
+    @Test
+    void testGetCommandData4() {
+        assertEquals(registerCommand.getCommandData().getOptions().get(2).getName(), "role");
+        assertEquals(
+                registerCommand.getCommandData().getOptions().get(2).getDescription(),
+                "Your role: one of (student, ta, professor)");
     }
 }
